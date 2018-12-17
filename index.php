f37844a6081222801736e1c8d0d3ee84dccacf2c
romualdhournon66 <k2001@cegetel.net>
2018-12-17 20:54:34 +0100

    mon index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="FR">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <title>PHP Partie 7 exercice 3</title>
+    </head>
+    <body>
+        <div class="container" id="page-top">
+            <div class="row">
+                <div class="col-xl-12">
+                    <h1 align="center">PHP Partie 7 exercice 3</h1>
+                    <h3 align="center">Exercice 3</h3>
+                    <p align="center">Avec le formulaire de l'exercice 1, afficher dans la page user.php les données du formulaire transmis.</p>
+                </div>
+            </div>
+            <form method="GET" action="user.php">
+                <div align="center"><input type="text" placeholder="Votre nom" name="Nom" value="Votre nom"/></div>
+                <div align="center"><input type="text" placeholder="Votre prénom" name="Prenom" value="Votre prénom"/></div>
+                <div align="center"><input type="submit" value="Valider" /></div>
+            </form>
+        </div>
+    </body>
+</html>
+
+
